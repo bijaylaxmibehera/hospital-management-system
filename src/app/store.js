15535262784febcef1d35b61a49fb17ea604bbbf
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-02-12 20:06:29 +0530

    feat: add hospital slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {patientSlice} from '../features/patients/patientSlice';
 import {wardSlice} from '../features/ward/wardSlice';
+import {hospitalSlice} from '../features/hospital/hospitalSlice';
 
 export const store = configureStore({
   reducer: {
     patients:patientSlice.reducer,
-    wards:wardSlice.reducer
+    wards:wardSlice.reducer,
+    hospital:hospitalSlice.reducer
   },
 });
