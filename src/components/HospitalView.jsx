2eb1690109600774ad6d08cd699b6f75082090bc
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-02-13 15:45:56 +0530

    feat: hospital view complete

--- src/components/HospitalView.jsx
+++ src/components/HospitalView.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setTopWard,
+  updateHospitalStats,
+} from "../features/hospital/hospitalSlice";
 
-export const HospitalView=()=>{
-    return (
-        <>
-        <h2>Hospital view</h2>
-        </>
-    )
-}
+export const HospitalView = () => {
+  const dispatch = useDispatch();
+  const hospitalStats = useSelector((state) => state.hospital);
+  const patients = useSelector((state) => state.patients.patients);
+  const wards = useSelector((state) => state.wards.wards);
+
+  useEffect(() => {
+    const totalPatients = patients.length;
+    const totalCapacity = wards.reduce(
+      (acc, curr) => acc + curr.bedsCapacity,
+      0
+    );
+    let occupancyRate;
+    if (totalCapacity) {
+      occupancyRate = (patients.length / totalCapacity) * 100;
+    } else {
+      occupancyRate = 0;
+    }
+    const wardCount = patients.reduce((acc, { assignedWard }) => {
+      acc[assignedWard] = (acc[assignedWard] ?? 0) + 1;
+      return acc;
+    }, {});
+
+    let maxCount = 0;
+    let topWard = null;
+    for (const [wardNumber, count] of Object.entries(wardCount)) {
+      if (count > maxCount) {
+        maxCount = count;
+        topWard = wardNumber;
+      }
+    }
+
+    dispatch(updateHospitalStats({ totalPatients, occupancyRate, topWard }));
+    dispatch(setTopWard(topWard));
+  }, [patients, wards, dispatch]);
+
+  return (
+    <div className="max-w-md mx-auto mt-8 p-6 bg-white shadow-md rounded-lg">
+      <h2 className="text-xl font-semibold mb-4">Hospital Stats</h2>
+      <div className="*mb-2">
+        <p>
+          <span>Total Patients: </span>
+          {hospitalStats.totalPatients}
+        </p>
+        <p>
+          <span>Current occupancy rate: </span>
+          {hospitalStats.occupancyRate}%
+        </p>
+        <p>
+          <span>Top ward: </span>
+          {hospitalStats.topWard ? hospitalStats.topWard : "-"}
+        </p>
+      </div>
+    </div>
+  );
+};
